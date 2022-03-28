fd9bd6f41f96ce4a8fea001001c2271250e3181f
saifullah rahman <saiflll284@gmail.com>
2022-03-29 01:02:49 +0530

    test

--- server.js
+++ server.js
@@ -17,12 +17,12 @@ const fe =
     ? "https://crime-tip-off-frontend.vercel.app"
     : "http://localhost:3000";
 
-// app.use(
-//   cors({
-//     credentials: true,
-//     origin: "https://crime-tip-off-frontend.vercel.app",
-//   })
-// );
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:3000",
+  })
+);
 
 app.use(cors());
 
